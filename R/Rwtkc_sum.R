2d84c482063186dddd3578249861aa839d276342
Rwtkc <rwtkc@foxmail.com>
2025-01-12 15:31:27 +0800

    Initial commit

---
+++ R/Rwtkc_sum.R
@@ -0,0 +1,14 @@
+
+#' @title Sum function for Rwtkc.
+#' @description This is an addition function developed by Rwtkc for testing purposes.
+#' @param x x must be a numeric value, serving as the addend.
+#' @param y y must be a numeric value, serving as the augend.
+#'
+#' @returns The returned value is the sum of x and y.
+#' @export
+#'
+#' @examples Rwtkc_sum(1,2)
+Rwtkc_sum <- function(x, y){
+  sum <- x + y
+  return(sum)
+}
